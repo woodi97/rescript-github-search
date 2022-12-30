bc1ce6001eba8273db282d42c8ed0e429a923932
woodi97 <woodi.daily@gmail.com>
2022-12-30 15:57:21 +0900

    fix: add config for tailwindcss

--- relay.config.js
+++ relay.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   src: "./src",
   schema: "./schema.graphql",
+  exclude: [
+    "**/node_modules/**",
+    "**/__mocks__/**",
+    "**/__generated__/**",
+    "**/.next/**",
+  ],
   artifactDirectory: "./src/__generated__",
 };
