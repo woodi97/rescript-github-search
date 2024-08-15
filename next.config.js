3870f2101f63a5ef0e5595f4e4a0e0845af555a4
woody-morgan <woody.morgan.crafts@gmail.com>
2024-08-16 01:50:30 +0900

    fix: remove wrong next.config found error

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const withPlugins = require("next-compose-plugins");
 const bsconfig = require("./bsconfig.json");
+const withImages = require("next-images");
 
 const transpileModules = ["rescript"]
 .concat(bsconfig["bs-dependencies"])
 
 const withTM = require("next-transpile-modules")(transpileModules);
 
-const isWebpack5 = true;
 const config = {
-  target: "serverless",
   pageExtensions: ["jsx", "js"],
   env: {
     ENV: process.env.NODE_ENV,
@@ -17,38 +16,37 @@ const config = {
   webpack: (config, options) => {
     const { isServer } = options;
 
-    if (isWebpack5) {
-      if (!isServer) {
-        // We shim fs for things like the blog slugs component
-        // where we need fs access in the server-side part
-        config.resolve.fallback = {
-          fs: false,
-          path: false,
-        };
-      }
-
-      // We need this additional rule to make sure that mjs files are
-      // correctly detected within our src/ folder
-      config.module.rules.push({
-        test: /\.m?js$/,
-        use: options.defaultLoaders.babel,
-        exclude: /node_modules/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
-        }
-      });
+    if (!isServer) {
+      // We shim fs for things like the blog slugs component
+      // where we need fs access in the server-side part
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+        process: false,
+      };
     }
+
+    config.module.rules.push({
+      test: /\.m?js$/,
+      use: options.defaultLoaders.babel,
+      exclude: /node_modules/,
+      type: "javascript/auto",
+      resolve: {
+        fullySpecified: false,
+      },
+    });
     return config
   },
-  future: {
-    webpack5: isWebpack5
-  }
+  webpack5: true,
+  reactStrictMode: true
 };
 
 module.exports = withPlugins(
   [
     [withTM],
+    [
+      withImages,
+    ]
   ],
   config
 );
