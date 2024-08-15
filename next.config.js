05305400254b1b914c32d566cd964986e5d5231f
woody-morgan <woody.morgan.crafts@gmail.com>
2024-08-16 01:50:30 +0900

    chore: do setting for rescript, relay, tailwindcss, nextjs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  pageExtensions: ['jsx', 'js', 'bs.js'],
 }
 
 module.exports = nextConfig
